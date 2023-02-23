8d840635428f1fb07a75fd568b09d0faa9463e04
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-02-23 11:23:00 +0900

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import http from "http";
 type ServerResponse = http.ServerResponse<http.IncomingMessage> & {
   req: http.IncomingMessage;
 };
-
 // NodeJs Server
-const server = http.createServer((_, res: ServerResponse) => {
+const server = http.createServer();
+
+const doRequest = (_: any, res: ServerResponse) => {
   // NOTE: Content-typeというヘッダー情報に「application.json」という値を設定
   res.writeHead(200, { "Content-Type": "application.json" });
   // NOTE: ボディ部分のコンテンツを書き出し
@@ -15,8 +16,13 @@ const server = http.createServer((_, res: ServerResponse) => {
     })
   );
   res.end();
-});
+};
 
+server.on("request", doRequest);
 server.listen(8000, () => {
   console.log("Application is running on: http://localhost:8000.");
 });
+
+
+
+
