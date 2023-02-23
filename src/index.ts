c338e730ed114579affc95a78da9c0c4314c415b
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-02-23 11:20:24 +0900

    feat very simple request

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-const msg = ():void => {
-    console.log("hello world")
-}
-msg();
+import http from "http";
+
+type ServerResponse = http.ServerResponse<http.IncomingMessage> & {
+  req: http.IncomingMessage;
+};
+
+// NodeJs Server
+const server = http.createServer((_, res: ServerResponse) => {
+  // NOTE: Content-typeというヘッダー情報に「application.json」という値を設定
+  res.writeHead(200, { "Content-Type": "application.json" });
+  // NOTE: ボディ部分のコンテンツを書き出し
+  res.write(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+  res.end();
+});
+
+server.listen(8000, () => {
+  console.log("Application is running on: http://localhost:8000.");
+});
