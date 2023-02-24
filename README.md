f2154d01c7ea4ad6837dacf6cf8cc0ac35373131
Ren <96362223+ren-yamanashi@users.noreply.github.com>
2023-02-24 19:30:11 +0900

    レビュー修正 (#8)
    * after review
    
    * delete git dist and node_module
    
    ---------
    
    Co-authored-by: ren_yamanashi <ren_yamanashi@mates-system.com>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-- express git hub: https://github.com/expressjs/express
+## References
+- Express git hub: https://github.com/expressjs/express
+- Node.Js documents: https://nodejs.org/docs
+
+## Quick Start
+- yarn start:dev
+
--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const application_1 = require("./application");
-const PORT = 8000;
-const app = new application_1.TypeExpress();
-app.listen(PORT);
-app.get();
