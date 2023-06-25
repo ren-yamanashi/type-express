8c81dffe32054542a0d8965fd744a3832687f1bb
ren_yamanashi <ren_yamanashi@mates-system.com>
2023-06-25 13:42:46 +0900

    feat auto change header 'res.sendFile' #45

---
+++ src/core/response/constance/index.ts
@@ -0,0 +1,9 @@
+export const MIME_TYPES = {
+  '.html': 'text/html',
+  '.js': 'text/javascript',
+  '.json': 'application/json',
+  '.css': 'text/css',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+} as const;
--- src/interfaces/fileSystem.ts
+++ src/interfaces/fileSystem.ts
@@ -1,5 +1,11 @@
-export type PathLike = string | Buffer | URL;
-export type BufferEncoding =
+type PathLike = string | Buffer | URL;
+type ErrnoException = {
+  errno?: number | undefined;
+  code?: string | undefined;
+  path?: string | undefined;
+  syscall?: string | undefined;
+};
+type BufferEncoding =
   | 'ascii'
   | 'utf8'
   | 'utf-8'
@@ -10,6 +16,39 @@ export type BufferEncoding =
   | 'latin1'
   | 'binary'
   | 'hex';
+
+interface StatsBase<T> {
+  isFile(): boolean;
+  isDirectory(): boolean;
+  isBlockDevice(): boolean;
+  isCharacterDevice(): boolean;
+  isSymbolicLink(): boolean;
+  isFIFO(): boolean;
+  isSocket(): boolean;
+  dev: T;
+  ino: T;
+  mode: T;
+  nlink: T;
+  uid: T;
+  gid: T;
+  rdev: T;
+  size: T;
+  blksize: T;
+  blocks: T;
+  atimeMs: T;
+  mtimeMs: T;
+  ctimeMs: T;
+  birthtimeMs: T;
+  atime: Date;
+  mtime: Date;
+  ctime: Date;
+  birthtime: Date;
+}
+
 export interface FileSystemInterface {
   readFile(path: PathLike, options?: BufferEncoding | null): Promise<string | Buffer>;
+  stat(
+    path: PathLike,
+    callback: (err: ErrnoException | null, stats: StatsBase<number>) => void,
+  ): void;
 }
